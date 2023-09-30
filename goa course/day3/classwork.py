5a9a84c26ffa7c23987a3985d5fd97fb752ad704
ninosolomonia <143136577+ninosolomonia@users.noreply.github.com>
2023-09-30 19:34:31 +0200

    Add files via upload

---
+++ goa course/day3/classwork.py
@@ -0,0 +1,13 @@
+#print("name")
+name = "tom"
+salary = 75000
+
+#here we learned about debagging
+#message ="debagging"
+#print(message)
+user_name = input("whats your name")
+print("hello" + user_name)
+#print(transaction)
+#new_budget = budget + 20
+#budget =200
+#print(new_budget)
---
+++ goa course/day3/homework.py
@@ -0,0 +1,11 @@
+
+
+#name=input("whats your name?")
+#surname=input("what`s your surname?")
+#age=input("whats your age?")              
+
+#print(name + " " + surname + " " +str(age)+ " " + "wlis")
+age=int(input("whats your age?"))
+age+=25
+print(age)
+print("after 25 years you will be  " + str(age) )
