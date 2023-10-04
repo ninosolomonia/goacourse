f47fdba54a3255aec4ab219f844502e0dd784a82
ninosolomonia <143136577+ninosolomonia@users.noreply.github.com>
2023-10-04 16:52:07 +0200

    Add files via upload

---
+++ goa course/day4/classwork.py
@@ -0,0 +1,17 @@
+a=4
+b=2
+c=a/b
+#birth_year = input(5)
+#print(type(birth_year))
+x = "55" #x is a string
+y = int(x) #y is an integer
+#height=input("whats your height")
+#print(height+ "me")
+#print(30<25)
+# flow of instructions a computer follows, using 3 techniques: sequencing, iteration and selection.
+#sequencing- the computer runs your code in order, from top to bottom.
+#Iteration-executing an instruction repeatedly. Iteration is commonly represented as a loop
+#Selection specifies when to follow each path.
+#An algorithm is a set of step-by-step instructions to complete a task, placed in a certain order.
+#flowcharts help to visualize algorithms.
+#Comparison and logical operations are needed to control the flow of your programs.
---
+++ goa course/day4/homework.py
@@ -0,0 +1,12 @@
+#davaleba1:
+#age=int(input("How old are you?")
+#print(age*7/3+4*9)
+#davaleba2:
+#car_price=7000
+#print(car_price*62/100)
+#davaleba3:
+client="99 xinkali momecit."
+manager="99 ra,barem 100 aige!"
+client1="100 ra,gori ki ar var"
+print("kaci shedis saxinkleshi da eubneba: "+client+manager+client1)
+
