52dd21a40b0fe80ca236c296f037241148f2a68c
ninosolomonia <143136577+ninosolomonia@users.noreply.github.com>
2023-10-10 17:58:35 +0200

    Add files via upload

---
+++ goa course/day5/homework1.py
@@ -0,0 +1,19 @@
+#i=2
+#while i<=30:
+    #print(str(i)+" luwia")
+    #i=i+2
+
+#i=1
+#while i<=30:
+    #print(str(i)+" kentia")
+    #i=i+2
+    
+for i in range(1,30,2):
+    print(str(i)+ "kentia")
+    print(str(i+1)+"luwia")
+
+
+    
+
+
+
