ad0fa76b8cc000a702bc3a242238b353f1c30466
ninosolomonia <143136577+ninosolomonia@users.noreply.github.com>
2023-10-10 17:57:22 +0200

    Delete goa course/day5 directory

--- goa course/day5/homework.py
+++
@@ -1,6 +0,0 @@
-age=int(input("how old are you?"))
-fathers_age=int(input("how old is your father?"))
-year=int(input("enter year"))
-for i in range(50):
-    print((fathers_age+i)/(age+ i))
-    print("in " + str(year+i)+ " my father will be  "+ str((fathers_age+i)/(age+i))+ " times older than me. ")
